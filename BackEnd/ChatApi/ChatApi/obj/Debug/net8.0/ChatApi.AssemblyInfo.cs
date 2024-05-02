6f65b86fa3b2bbede68c4a3d103fce8f0197c8d6
Tiziano Titone <titone1996@gmail.com>
2024-05-02 11:33:00 +0200

    Aggiunta DTO

--- BackEnd/ChatApi/ChatApi/obj/Debug/net8.0/ChatApi.AssemblyInfo.cs
+++ BackEnd/ChatApi/ChatApi/obj/Debug/net8.0/ChatApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fdcc237f16d037d9284bf37a6466917a031bd4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d767a7413534e221d7c4580ff22906040d477d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
