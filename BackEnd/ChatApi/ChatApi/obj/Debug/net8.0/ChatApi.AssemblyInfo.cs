7e7d9fdf89244fbd049b8df2013f166465b33837
andreas ubertini <ubertiniandreas@gmail.com>
2024-05-03 17:12:49 +0200

    ChatRoomCustom

--- BackEnd/ChatApi/ChatApi/obj/Debug/net8.0/ChatApi.AssemblyInfo.cs
+++ BackEnd/ChatApi/ChatApi/obj/Debug/net8.0/ChatApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d1c86741c247996e7a10c54b762af078694328")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a6ef8ab84d5c2b58400c5b29c13ed60a548e391")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
